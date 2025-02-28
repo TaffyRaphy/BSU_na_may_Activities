5a36ac8b1e482b07a41d1817da1fba29e6cf6286
Raphy Igot <igotraphael@gmail.com>
2025-02-28 19:10:09 +0800

    whoopss please ignore the variable names HAHAH

---
+++ CC104/Activity3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Activity3 {
+    public static void main(String[] args) {
+        int[] arawkopo = new int[10];
+        Scanner input = new Scanner(System.in);
+        int kots;
+
+        System.out.println("putanginang code re o");
+        for (kots = 0; kots < 10; kots++) {
+            System.out.print("nyeyeye input here fucker ka");
+            arawkopo[kots] = input.nextInt();
+        }
+
+        // eto o tangina titignan na AAAAAAAAAAAAAA
+        int[] tanginamooo = new int[10];
+        int fuckyo = 0;
+        for (kots = 0; kots < 10; kots++) {
+            if (arawkopo[kots] != 0) {
+                tanginamooo[fuckyo] = arawkopo[kots];
+                fuckyo++;
+            }
+        }
+
+        while (fuckyo < 10) {
+            tanginamooo[fuckyo] = 0;
+            fuckyo++;
+        }
+
+        for (kots = 0; kots < 10; kots++) {
+            arawkopo[kots] = tanginamooo[kots];
+        }
+
+        for (kots = 0; kots < 10; kots++) {
+            System.out.print(arawkopo[kots] + " ");
+        }
+    }
+}
