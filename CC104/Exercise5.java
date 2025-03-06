61cc4749bf311053f916f1a9e8cb0275fbd99f71
Raphy Igot <igotraphael@gmail.com>
2025-03-06 17:47:29 +0800

    teka bossing ginagawa kopa

---
+++ CC104/Exercise5.java
@@ -0,0 +1,8 @@
+package CC104;
+import java.util.Scanner;
+public class Exercise5 {
+    Scanner input = new Scanner(System.in);
+    String omsim;
+    System.out.println("Please input a single word, then the program will manipulate it.");
+    omsim = input.nextLine();
+}
